8d915ad7f8cfcc6dc5103a21e97d35fe2abc16b3
robclu <robclu1818@gmail.com>
2015-08-13 13:00:59 +0200

    Started tensor classes

---
+++ include/tensor_expressions.hpp
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------------------------------------
+/// @file   Header file for ftl tensor expression classes.
+// ----------------------------------------------------------------------------------------------------------
+
+//
+//	NOTES:
+//		- The following conventions are used for brevity
+//			: E		: Means Expression when used as a template parameter
+//
+
+#ifndef FTL_TENSOR_EXPRESSIONS
+#define FTL_TENSOR_EXPRESSIONS
+
+namespace ftl {
+
+// ---
+// ---
+template <typename Expression
+}		// End namespace ftl
+
+#endif	// FTL_TENSOR_EXPRESSIONS
